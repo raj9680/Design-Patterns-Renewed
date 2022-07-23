4b93333f2857e400dae86f9007bc13b8062f5622
raj kumar <raj.kumar@kochartech.corp>
2022-07-23 14:55:09 +0530

    inits

---
+++ StrategyPattern_AutomateFactoryPatternUsingUnity/Factory_Rip_LazyLoading/FactoryCustomer/Class1.cs
@@ -0,0 +1,42 @@
+﻿using MiddleLayer;
+using System;
+using System.Collections.Generic;
+
+namespace FactoryCustomer
+{
+    public static class Factory // Simple Factory Pattern (Manual)
+    {
+        private static Dictionary<string, CustomerBase> custs = new Dictionary<string, CustomerBase>();
+
+        /// Constructor - XX
+        // static Factory()      // It is creating object whether required or not required, which is not good.
+        //                      // To make it instantiate only when required we use Lazy Loading Pattern. - XXX
+        // {
+        //     custs.Add("Customer", new Customer());
+        //     custs.Add("Lead", new Lead());
+        // }
+
+        public static CustomerBase Create(string TypeCust)
+        {
+            // Lazy Loading Design Pattern -> opposite of lazy loading is eager loading
+            if(custs.Count == 0)
+            {
+                custs.Add("Customer", new Customer());
+                custs.Add("Lead", new Lead());
+            }
+
+            // Get this implementation from FrmCustomer.cs - later removed and made bcoz of multiple if using - XX
+            /// if(TypeCust == "Customer")
+            /// {
+            ///     return new Customer();
+            /// }
+            /// else
+            /// {
+            ///     return new Lead();
+            /// }
+            
+            // RIP - Replace if with Polymorphism
+            return custs[TypeCust];
+        }
+    }
+}
---
+++ StrategyPattern_AutomateFactoryPatternUsingUnity/Factory_Rip_LazyLoading/WinFormCustomer/FrmCustomer.cs
@@ -0,0 +1,99 @@
+﻿using MiddleLayer;
+using System;
+using System.Windows.Forms;
+using FactoryCustomer;
+
+namespace WinFormCustomer
+{
+    public partial class FrmCustomer : Form
+    {
+        /// private Customer cust = null;   private Lead lead = null;
+        // Common object creation for both Customer & Lead -- Polymorphism(change as per situation)
+        private CustomerBase Cust = null;    // - X
+        public FrmCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void BillDateLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CustomerType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // We are creating objects here, best approach to have centralized class for all object creation (Factory Design Pattern) so
+            // moved that part to another class/project FactoryCustomer
+            /// if (CustomerType.Text == "Customer")
+            /// {
+            ///    Cust = new Customer();
+            /// }
+            /// else
+            /// {
+            ///     Cust = new Lead();
+            /// }
+
+            Cust = Factory.Create(CustomerType.Text);
+        }
+
+        private void SetCustomer()
+        {
+            Cust.CustomerName = CustomerName.Text;
+            Cust.PhoneNumber = PhoneNumber.Text;
+            Cust.BillAmount = Convert.ToDecimal(BillAmount.Text);
+            Cust.BillDate = Convert.ToDateTime(BillDate.Text);
+            Cust.Address = Address.Text;
+        }
+
+        private void ValidateBtn_Click(object sender, EventArgs e)
+        {
+            /// Object Creation
+            // if(CustomerType.Text == "Customer")
+            // {
+            //     Cust.Validate();
+            // }
+            // else
+            // {
+            //     Cust.Validate();
+            // }
+
+            // Because of X we can use only below approach instead of above approach
+            try
+            {
+                SetCustomer();
+                Cust.Validate();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+            }
+            
+        }
+
+        private void CustomerName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BillAmount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BillDate_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Address_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
